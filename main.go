9c33730e1d4794d8562acab4df433da3e8825125
root <root@localhost.localdomain>
2016-02-21 06:27:35 -0800

    interactive game

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "net/http"
+import "fmt"
+import "os"
+import "io"
+import "ws"
+
+func main() {
+	println("hel")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("fedorashared/ws.html")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		io.Copy(w, f)
+	})
+	http.HandleFunc("/mv", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("fedorashared/mv.html")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		io.Copy(w, f)
+	})
+	go func() {
+		err := http.ListenAndServe(":80", nil)
+		println("ehll")
+		fmt.Println(err.Error())
+	}()
+	err := http.ListenAndServe(":81", ws.WsHandler)
+	fmt.Println(err.Error())
+}
