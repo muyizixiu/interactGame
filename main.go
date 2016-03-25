939293808fcd8513372287f8444d408bdba2fbaa
root <root@localhost.localdomain>
2016-03-25 22:37:47 +0800

    a new game

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package main /*package main
 
 import (
 	"encoding/json"
@@ -94,4 +94,60 @@ func main() {
 	println("server listen at 81 port")
 	err := http.ListenAndServe(":81", ws.WsHandler)
 	fmt.Println(err.Error())
+}
+*/
+import (
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+import "fmt"
+
+import "ws"
+
+func main() {
+	go func() {
+		println("server listen at 80 port")
+		err := http.ListenAndServe(":80", httpHandler)
+		fmt.Println(err.Error())
+	}()
+	println("server listen at 81 port")
+	err := http.ListenAndServe(":81", ws.WsHandler)
+	fmt.Println(err.Error())
+}
+
+// http handler
+type Handler struct {
+	Router map[string]string
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	println(r.URL.Path)
+	h.Route(r.URL.Path, w, r)
+}
+
+func (h Handler) Route(path string, w http.ResponseWriter, r *http.Request) {
+	path = strings.Trim(path, "/")
+	if str, ok := h.Router[path]; ok {
+		path = str
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		println(err.Error())
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer f.Close()
+	io.Copy(w, f)
+}
+
+var httpHandler Handler
+
+func init() {
+	httpHandler.Router = map[string]string{
+		"chess":         "chess.html",
+		"flappy":        "muyizixiu/flappy-block.html",
+		"flappy.mobile": "muyizixiu/flappy-mobile.html",
+	}
 }
