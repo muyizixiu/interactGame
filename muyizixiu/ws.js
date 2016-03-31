0a00caafb31f2c0ea8cb9178908ea8674bcad5b3
root <root@localhost.localdomain>
2016-03-31 14:08:26 +0800

    support bullet feature

--- muyizixiu/ws.js
+++ muyizixiu/ws.js
@@ -1,4 +1,4 @@
-var url="ws://"+IP+":81/chess"
+var url="ws://"+IP+":81/"+location.search;
 var ws=new WebSocket(url);
 	ws.onopen = function(event) { 
 
@@ -7,9 +7,9 @@ var ws=new WebSocket(url);
 
   // 监听消息
   ws.onmessage = function(event) { 
-    console.log('Client received a message',event); 
+    //console.log('Client received a message',event); 
     data=JSON.parse(event.data)
-    console.log(data)
+    //console.log(data)
     if(data.end){
     	//alert("your enemy has fleed away!");
 	lock=true;
@@ -17,14 +17,21 @@ var ws=new WebSocket(url);
     }
     if(data.error){
       console.log(data.error)
-    	 //window.location.href="http://"+IP+"/muyizixiu/ufo-war.html"; 
+    	 window.location.href="http://"+IP+"/muyizixiu/ufo-war.html?gName=chess";
 	 return
     }
     if(data.start){
       G.lock=false;
 	    //alert("the battle has been lauched!");
-	   alert("开始了，点击就送!")
+	   //alert("开始了，点击就送!")
      ufo.move();
+     enemy.move();
+     setTimeout(G.freshFrame,500);
+     return
     }
+    if(data.rid){
+      return
+    }
+    G.nextData.enemy=data.ufo;
   }
 }
